7f62b5f7f33fe2f4304f36c549528930d6020f03
ypeng <ypeng3@uvm.edu>
2020-11-24 21:39:39 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var all_buttons = document.getElementsByTagName("button");
+
+
+function Run(background, clr) {
+    background.style.color = clr;
+    console.log("run");
+
+}
+
+function Stop(background, clr) {
+    background.style.color = clr;
+    console.log("stop");
+}
